bc5d09da9a4bdc4d19fcfb1f25af6ae9dcfee7a4
GERALDINEwasas <rgguzman71@gmail.com>
2024-11-19 12:19:04 -0400

    javascript

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+let mensaje="lol"
+console.log(mensaje);
+
+
+function sumar(){
+    let numero1=3;
+    let numero2=4;
+    let suma=numero1+numero2;
+    alert("la suma es"+ suma);
+}
+function resta(){
+    let numero1=3;
+    let numero2=4;
+    let resta=numero1-numero2;
+    alert("la resta es"+ resta);
+}
+function multiplicacion(){
+    let numero1=3;
+    let numero2=4;
+    let multiplicacion=numero1*numero2;
+    alert("la multiplicacion es"+ multiplicacion);
+}
+
+function division(){
+    let numero1=3;
+    let numero2=4;
+    let division=numero1/numero2;
+    alert("la division es"+ division);
+}
+
